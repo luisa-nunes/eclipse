ed00e73b3c09f49c55dd57a3031c06cd72330574
luisa-nunes <97109693+luisa-nunes@users.noreply.github.com>
2022-04-15 13:42:47 +0100

    Added files from my compute

---
+++ Healthcare Scheduling/src/br/com/luisanunes/git/HelloScheduling.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.luisanunes.git;
+
+/**
+ * @author luisa
+ * Example of Eclipse integration with GitHub
+ */
+public class HelloScheduling {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hello Scheduling");
+
+	}
+
+}
